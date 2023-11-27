bd74ec5152f9902358912f00bbc29dfeb3371495
Oskar Nizioł <oskarniziol@gmail.com>
2023-11-27 18:04:37 +0100

    Add error to logger

--- src/Utils/Logger.php
+++ src/Utils/Logger.php
@@ -6,4 +6,8 @@ class Logger
     public static function info(string $str) : void {
         echo "\033[36m INFO \033[0m $str \n";
     }
+
+    public static function error(string $str) : void {
+        echo "\033[91m ERROR \033[0m $str \n";
+    }
 }
