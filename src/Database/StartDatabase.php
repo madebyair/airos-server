008b2a76b3d85bb2b4eaea6f4172fcb23a58090f
Oskar Nizioł <oskarniziol@gmail.com>
2023-11-29 09:10:58 +0100

    Save password to file, and fix start database.

--- src/Database/StartDatabase.php
+++ src/Database/StartDatabase.php
@@ -9,25 +9,33 @@ class StartDatabase
     public function start(): void {
         Logger::info("Killing old database instance");
         $this->killOldDatabase();
-        $password = $this->generateRandomString();
+        $password = $this->loadPassword();
 
         DatabaseInformation::setPassword($password);
 
-        exec("/usr/server/thread_command 'surreal start file:///usr/server/database -A --auth --user root --pass $password --bind 127.0.0.1:1398'");
+        exec("/usr/server/thread_command 'surreal start -b 127.0.0.1:1398 file:///usr/server/database -A --auth --user root --pass $password'");
     }
 
-    // Credits: https://stackoverflow.com/questions/4356289/php-random-string-generator
-    private function generateRandomString($length = 40) {
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $charactersLength = strlen($characters);
-        $randomString = '';
-        for ($i = 0; $i < $length; $i++) {
-            $randomString .= $characters[random_int(0, $charactersLength - 1)];
-        }
-        return $randomString;
-    }
 
     private function killOldDatabase(): void {
-        exec("kill-port 1398");
+        shell_exec("/usr/server/kill 1398");
+    }
+
+    private function loadPassword(): string {
+        if(!file_exists("/usr/server/pass")) {
+            Logger::error("Database password (/usr/server/pass) are empty. Please put new database password, to continue.");
+            exit(1);
+        }
+
+        if (filesize("/usr/server/pass") == 0) {
+            Logger::error("Database password (/usr/server/pass) are empty. Please put new database password, to continue.");
+            exit(1);
+        } else {
+            $file = fopen("/usr/server/pass", "r");
+            $content = fread($file, filesize("/usr/server/pass"));
+            fclose($file);
+
+            return $content;
+        }
     }
 }
